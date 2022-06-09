1482f66b04b5f7e00b12af0cda5bb46cf016670e
Soumiya Ayouch <ayouch.somaya@gmail.com>
2022-06-09 10:21:15 +0100

    added database to login

--- php/login/login_session.php
+++ php/login/login_session.php
@@ -1,17 +1,31 @@
+<?php include 'database_login.php';?>
+
 <?php
   session_start();
 
    $email = filter_input(INPUT_POST,'user_email',
    FILTER_SANITIZE_EMAIL);
    $password= $_POST['user_password'];
+   $_SESSION['email'] = $email;
 
     if($email=='somaya@gmail.com' && $password =='password'){
-       echo '<h1>Welcome ' . 'Admin !' .'</h1>';
+       echo '<h1>Welcome Admin ! ' . '</h1>';
        echo '<a href="users.php">Users</a><br/>' ;
        echo '<a href="logout.php">logout</a>' ;
-    } else{
-       echo '<h1>Welcome Guest</h1>';
-       echo '<a href="login.html">Home</a>';   
-    }
+    }else{
+
+         $sql = "SELECT * FROM users WHERE user_email='$email' AND user_password='$password'";
+         $result = mysqli_query($conn,$sql);
+         $users = mysqli_fetch_all($result,MYSQLI_ASSOC);
+         if(!empty($users)){
+               foreach($users as $user){
+                  if($user['user_email'] == $_SESSION['email']){
+                     echo "<h1>Welcome ".$user['user_first_name'].' '.$user['user_last_name']. "</h1>";
+                  }else{
+                     echo '<h1>User does not exist</h1>';
+                  }
+               }
+         }
+      }
 
 ?>
